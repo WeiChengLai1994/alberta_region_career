e8328ec874e8f5dcbb5107f7cd028b4b771da72b
Tzu-Yun Huang <alicehuang.inca@gmail.com>
2024-11-28 21:54:32 -0700

    1128 dashboard

--- app/pages/dashboard/employer/page.js
+++ app/pages/dashboard/employer/page.js
@@ -34,7 +34,7 @@ const EmployerDashboard = () => {
   return (
     <div className="flex h-screen">
       {/* left hand side menu */}
-      <div className="bg-gray-200 p-6 flex flex-col space-y-4">
+      <div className="bg-gray-200 p-6 flex flex-col space-y-10">
         <h1 className="text-2xl font-bold">Employer Dashboard</h1>
       <Link href="/" className="flex items-center space-x-2">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -96,15 +96,27 @@ const EmployerDashboard = () => {
         <div className="bg-white rounded-lg shadow-2xl p-4 mb-8">
           <h3 className="text-lg font-medium mb-4">Application Received</h3>
           <div className="flex flex-col justify-center items-center h-64">
-            <div className="flex flex-col justify-center items-center h-64">
-          <img src="../../../public/image/employerDashboard_dataExample.png" alt="Example Image" className="max-w-full h-auto" />
-        </div>
+            <div className="flex flex-col justify-center items-center h-64 ">
+              <img src="../../../../..//image/employerDashboard_dataExample.png" alt="Example Image" className="max-w-full h-auto " />
+            </div>
           </div>
         </div>
 
-        <div className="bg-white rounded-xl shadow-2xl p-5">
+        <div className="bg-white rounded-xl shadow-md p-5">
           <h3 className="text-lg font-medium mb-4">Posted Jobs</h3>
           <table className="w-full">
+            <thead>
+              {/* <tr className="bg-gray-300">
+                <th className="py-3 px-4 text-left font-medium">
+                  <div className="flex items-center">
+                    <div className="w-12 h-12 rounded-md bg-gray-200 mr-4"></div>
+                    Title
+                  </div>
+                </th>
+                <th className="py-3 px-4 text-left font-medium">Type</th>
+                <th className="py-3 px-4 text-left font-medium">Location</th>
+              </tr> */}
+            </thead>
             <tbody>
               {dashboardData.postedJobsList.map((job, index) => (
                 <tr
@@ -114,14 +126,26 @@ const EmployerDashboard = () => {
                   } border-b border-gray-200`}
                 >
                   <td className="py-3 px-4">
-                    <div className="space-y-1">
-                      <span className="font-medium">{job.title}</span>
-                      <div className="flex items-center space-x-4 text-sm text-gray-500">
-                        <span>{job.type}</span>
-                        <span>{job.location}</span>
+                    <div className="flex items-center">
+                      <div className="w-12 h-12 rounded-md bg-gray-200 mr-4"></div>
+                      <div>
+                        <span className="font-medium">{job.title}</span>
+                        <div className="flex items-center space-x-4 text-sm text-gray-500">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                          </svg>
+                          <span>{job.type}</span>
+                          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                          </svg>
+                          <span>{job.location}</span>
+                        </div>
                       </div>
                     </div>
                   </td>
+                  {/* <td className="py-3 px-4 text-sm">{job.type}</td>
+                  <td className="py-3 px-4 text-sm">{job.location}</td> */}
                 </tr>
               ))}
             </tbody>
