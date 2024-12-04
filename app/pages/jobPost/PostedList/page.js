48d5164855e8fbef3df544af23ff0e4d8cd04a68
Tzu-Yun Huang <alicehuang.inca@gmail.com>
2024-12-04 13:42:45 -0700

    1204 style and comment

--- app/pages/jobPost/PostedList/page.js
+++ app/pages/jobPost/PostedList/page.js
@@ -118,18 +118,18 @@ const PostedList = () => {
       }
     ],
   };
-  const [currentPage, setCurrentPage] = useState(1); // 當前頁碼
-  const jobsPerPage = 6; // 每頁顯示的工作數量
+  const [currentPage, setCurrentPage] = useState(1); // Current page number
+  const jobsPerPage = 6; // Every page shows 6 jobs
 
-  // 計算總頁數
+  // Calculate the total number of pages
   const totalPages = Math.ceil(dashboardData.postedJobsList.length / jobsPerPage);
 
-  // 根據當前頁碼計算需要顯示的工作
+  // Calculate the work to be displayed based on the current page number
   const indexOfLastJob = currentPage * jobsPerPage;
   const indexOfFirstJob = indexOfLastJob - jobsPerPage;
   const currentJobs = dashboardData.postedJobsList.slice(indexOfFirstJob, indexOfLastJob);
 
-  // 處理頁碼切換
+  // Page Change
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
@@ -236,8 +236,8 @@ const PostedList = () => {
             {currentJobs.map((job, index) => (
               <div key={index} className="bg-white shadow-md rounded-lg p-6 mb-6 border border-gray-200 ">
                 <div className="d-grid gap-2 d-md-block grid grid-cols-2 mb-10">
-                    <button className="btn btn-primary bg-[#325F66] text-white px-8 py-3 rounded-lg text-lg mr-2" type="button">EDIT</button>
-                    <button className="btn btn-primary bg-gray-400 text-white px-8 py-3 rounded-lg text-lg mr-2" type="button">DELETE</button>
+                    <button className="btn btn-primary bg-[#325F66] text-white px-8 py-3 rounded-lg text-lg mr-2 font-bold" type="button">EDIT</button>
+                    <button className="btn btn-primary bg-gray-400 text-white px-8 py-3 rounded-lg text-lg mr-2 font-bold" type="button">DELETE</button>
                 </div>
                 
                     <h2 className="text-2xl font-bold mb-4">{job.title}</h2>
@@ -245,22 +245,22 @@ const PostedList = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                         </svg>
-                        <span className="text-gray-500 text-sm mr-4">{job.type}</span>
+                        <span className="text-gray-500 text-sm mr-4 font-bold">{job.type}</span>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
-                        <span className="text-gray-500 text-sm mr-4">{job.location}</span>
+                        <span className="text-gray-500 text-sm mr-4 font-bold">{job.location}</span>
 
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-500 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 9V7a2 2 0 00-2-2H9a2 2 0 00-2 2v2M3 13h18M5 17h14a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2z" />
                         </svg>
-                        <span className="text-gray-500 text-sm mr-4">{job.salary}</span>
+                        <span className="text-gray-500 text-sm mr-4 font-bold">{job.salary}</span>
 
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3M12 2a10 10 0 100 20 10 10 0 000-20z" />
                         </svg>
-                        <span className="text-gray-500 text-sm mr-4">{job.postTime}</span>
+                        <span className="text-gray-500 text-sm mr-4 font-bold">{job.postTime}</span>
 
                     </div>
                         <p className="text-gray-600 mb-4">{job.description}</p>
